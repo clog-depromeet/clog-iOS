628de5b8b96e6abbf6b9d1e9520cc69191f79f53
HyeonJun Kang <khjj9865@naver.com>
2025-05-23 18:24:25 +0900

    chore: PR리뷰 반영

---
+++ Clog/Projects/ClogService/Features/SocialFeature/Interface/Sources/SocialTabFeature.swift
@@ -0,0 +1,71 @@
+//
+//  SocialTabFeature.swift
+//  SocialFeature
+//
+//  Created by 강현준 on 5/23/25.
+//  Copyright © 2025 Supershy. All rights reserved.
+//
+
+import ComposableArchitecture
+import Core
+
+@Reducer
+public struct SocialTabFeature {
+    
+    public init() {}
+    
+    @ObservableState
+    public struct State: Equatable {
+        public var selectedTab: CurrentTab = .follower
+        
+        public init() { }
+    }
+    
+    public enum Action: BindableAction, FeatureAction, ViewAction {
+        case binding(BindingAction<State>)
+        case inner(InnerAction)
+        case async(AsyncAction)
+        case scope(ScopeAction)
+        case delegate(DelegateAction)
+        case view(View)
+    }
+    
+    public enum View {
+        case selectTab(SocialTabFeature.State.CurrentTab)
+    }
+    
+    public enum InnerAction { }
+    public enum AsyncAction { }
+    public enum ScopeAction { }
+    public enum DelegateAction { }
+    
+    public var body: some Reducer<State, Action> {
+        
+        BindingReducer()
+        
+        Reduce { state, action in
+            switch action {
+            case .view(let action):
+                return viewCore(&state, action)
+                
+            default:
+                return .none
+            }
+        }
+    }
+    
+    func viewCore(_ state: inout State, _ action: View) -> Effect<Action> {
+        switch action {
+        case .selectTab(let tab):
+            state.selectedTab = tab
+            return .none
+        }
+    }
+}
+
+public extension SocialTabFeature.State {
+    enum CurrentTab: String, Hashable {
+        case follower = "팔로워"
+        case following = "팔로잉"
+    }
+}
--- Clog/Projects/ClogService/Features/SocialFeature/Sources/Social/Components/SocialTabView.swift
+++ Clog/Projects/ClogService/Features/SocialFeature/Sources/Social/Components/SocialTabView.swift
@@ -9,49 +9,13 @@
 import SwiftUI
 import ComposableArchitecture
 import DesignKit
+import Core
+import SocialFeatureInterface
 
-@Reducer
-public struct SocialTabFeature {
-    
-    @ObservableState
-    public struct State: Equatable {
-        var selectedTab: CurrentTab = .follower
-    }
-    
-    public enum Action: BindableAction {
-        
-        case binding(BindingAction<State>)
-        
-        case selectTab(SocialTabFeature.State.CurrentTab)
-    }
-    
-    public var body: some Reducer<State, Action> {
-        
-        BindingReducer()
-        
-        Reduce { state, action in
-            switch action {
-            case .selectTab(let tab):
-                state.selectedTab = tab
-                return .none
-                
-            default:
-                return .none
-            }
-        }
-    }
-}
-
-public extension SocialTabFeature.State {
-    enum CurrentTab: String, Hashable {
-        case follower = "팔로워"
-        case following = "팔로잉"
-    }
-}
-
+@ViewAction(for: SocialTabFeature.self)
 public struct SocialTabView: View {
     
-    @Bindable private var store: StoreOf<SocialTabFeature>
+    @Bindable public var store: StoreOf<SocialTabFeature>
     
     public init(
         store: StoreOf<SocialTabFeature>
@@ -61,60 +25,64 @@ public struct SocialTabView: View {
     
     public var body: some View {
         VStack(spacing: 0) {
-            HStack(spacing: 0) {
-                
-                VStack(spacing: 0) {
-                    Button {
-                        store.send(.selectTab(.follower))
-                    } label: {
-                        HStack {
-                            Spacer()
-                            Text("팔로워")
-                                .font(.b1)
-                                .foregroundStyle(
-                                    store.selectedTab == .follower
-                                    ? Color.clogUI.primary
-                                    : Color.clogUI.gray400
-                                )
-                            Spacer()
-                        }
-                    }
-                    
-                    DividerView(
-                        .horizontal,
-                        color: store.selectedTab == .follower
-                        ? .clogUI.primary
-                        : .clear
-                    )
-                }
-                
-                VStack(spacing: 0) {
-                    Button {
-                        store.send(.selectTab(.following))
-                    } label: {
-                        HStack {
-                            Spacer()
-                            Text("팔로잉")
-                                .font(.b1)
-                                .foregroundStyle(
-                                    store.selectedTab == .following
-                                    ? Color.clogUI.primary
-                                    : Color.clogUI.gray400
-                                )
-                            Spacer()
-                        }
-                    }
-                    
-                    DividerView(
-                        .horizontal,
-                        color: store.selectedTab == .following
-                        ? .clogUI.primary
-                        : .clear
-                    )
-                }
-            }
+            followFollowingButtonView
             TabView(selection: $store.selectedTab) {
                
+            }
+        }
+    }
+    
+    private var followFollowingButtonView: some View {
+        HStack(spacing: 0) {
+            
+            VStack(spacing: 0) {
+                Button {
+                    send(.selectTab(.follower))
+                } label: {
+                    HStack {
+                        Spacer()
+                        Text("팔로워")
+                            .font(.b1)
+                            .foregroundStyle(
+                                store.selectedTab == .follower
+                                ? Color.clogUI.primary
+                                : Color.clogUI.gray400
+                            )
+                        Spacer()
+                    }
+                }
+                
+                DividerView(
+                    .horizontal,
+                    color: store.selectedTab == .follower
+                    ? .clogUI.primary
+                    : .clear
+                )
+            }
+            
+            VStack(spacing: 0) {
+                Button {
+                    send(.selectTab(.following))
+                } label: {
+                    HStack {
+                        Spacer()
+                        Text("팔로잉")
+                            .font(.b1)
+                            .foregroundStyle(
+                                store.selectedTab == .following
+                                ? Color.clogUI.primary
+                                : Color.clogUI.gray400
+                            )
+                        Spacer()
+                    }
+                }
+                
+                DividerView(
+                    .horizontal,
+                    color: store.selectedTab == .following
+                    ? .clogUI.primary
+                    : .clear
+                )
             }
         }
     }
